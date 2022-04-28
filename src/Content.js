df3184a05dbca0a3986f888083d33cc9ff7031b0
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-28 16:39:41 +0700

    todo 3-5: add content

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,57 @@
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
 const Content = () => {
-  const handleNameChange = () => {
-    const names = ["FIryal", "Armin", "Levi"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  };
+  //TODO 2
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "cookies",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "bread",
+    },
+    {
+      id: 3,
+      checked: true,
+      item: "ice cream",
+    },
+  ]);
+
+
 
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      {items.length ? (
+        // {/* TODO 3 */}
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                // TODO 7
+                checked={item.checked}
+              ></input>
+              <label
+                style={item.checked ? { textDecoration: "line-through" } : null}
+              >
+                {item.item}
+              </label>
+              {/* TODO 4 */}
+              <FaTrashAlt
+                
+                role="button"
+                tabIndex="0"
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>The list is empty</p>
+      )}
     </main>
   );
 };
