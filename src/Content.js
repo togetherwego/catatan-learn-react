3b21f3be7d1c3920895dacbac07be553812832e1
firyalhuwaida <iyaalfiryal@gmail.com>
2022-05-09 14:19:57 +0700

    todo 8-10: add handleDelete and empty list

--- src/Content.js
+++ src/Content.js
@@ -31,8 +31,17 @@ const Content = () => {
     localStorage.setItem("shoppinglist", JSON.stringify(listItems));
   };
 
+  // todo 9
+  const handleDelete = (id) => {
+    console.log(id);
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
   return (
     <main>
+      {/* todo 10 */}
       {items.length ? (
         // {/* TODO 3 */}
         <ul>
@@ -45,6 +54,7 @@ const Content = () => {
                 checked={item.checked}
               ></input>
               <label
+                // todo 8
                 style={item.checked ? { textDecoration: "line-through" } : null}
                 onDoubleClick={() => handleCheck(item.id)}
               >
@@ -52,7 +62,7 @@ const Content = () => {
               </label>
               {/* TODO 4 */}
               <FaTrashAlt
-                
+                onClick={() => handleDelete(item.id)}
                 role="button"
                 tabIndex="0"
               />
